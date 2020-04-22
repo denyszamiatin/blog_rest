305d0c5eb6fff222299a62d0494ad49479c9938f
denyszamiatin <Celtic19771977>
2020-04-22 17:58:04 +0300

    Postgres

--- config.py
+++ config.py
@@ -8,22 +8,23 @@ BASE_DIR = pathlib.Path(__file__).parent
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     ITEMS_ON_PAGE = 2
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProdConfig(Config):
     DEBUG = False
     ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1@localhost:5432/postgres"
+    # SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/postgres"
 
 
 class DevConfig(Config):
     DEBUG = True
     ENV = 'development'
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(BASE_DIR / "data" / "db.sqlite3")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig(Config):
     DEBUG = True
     ENV = 'testing'
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
